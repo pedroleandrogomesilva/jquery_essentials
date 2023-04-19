038a6f7eb7bd0dc8650f938e718757792bf70746
pedroleandrogomesilva <pedroleandrogomesilva@gmail.com>
2023-04-19 15:24:11 -0300

    feat(iniciando com jquery): é preciso debugar

---
+++ 02-eh-preciso-debugar/script.js
@@ -0,0 +1,28 @@
+// Forma padrão simplificada
+
+var jqueryessentials = "Olá, Mundo! #BoraProgramar!"
+alert(jqueryessentials);
+$(function () {
+    $(".jquery_essentials").html("Jquery Essentials Inicializado!")
+})
+
+console.group("Mensagens gerais");
+
+console.log("jQuery Essentials!");
+console.info("jQuery foi inicializado com sucesso!");
+console.warn("Um alerta!");
+console.error("Um erro!");
+
+console.groupEnd();
+
+var cadastro = false;
+
+console.group("Cadastro de clientes");
+
+if(cadastro === true){
+    console.info("Cadastro iniciado!");
+}else{
+    console.warn("Erro ao cadastrar!")
+}
+
+console.groupEnd();
